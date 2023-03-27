86f6c4a08b56d787ce4755a0c10ff30b62303775
Meaghan Perdue <meaghan@lyra.healthy.bewell.ca>
2023-03-27 12:59:53 -0600

    add visualization step

--- src/3_dwi2response_dhollander.sh
+++ src/3_dwi2response_dhollander.sh
@@ -12,10 +12,13 @@ export mrtrix_out=/Volumes/catherine_team/Trainee_Folders/mvperdue/preschool/bid
 cd $mrtrix_out/${1}/${2}
 
 #run response function estimation
-dwi2response dhollander ${1}_${2}_dwi_preprocessed.mif wm_response.txt gm_response.txt csf_response.txt -voxels voxels.mif  -debug
+dwi2response dhollander ${1}_${2}_dwi_preprocessed.mif wm_response.txt gm_response.txt csf_response.txt -voxels voxels.mif  -info
+
+#optionally view outputs, check shapes by tissue type (sphere vs. flattened) at each bval
+#shview wm_response.txt
 
 #upsample DWI data for improvement of later template building, registration, tractography & stats (per mrtrix documentation)
-mrgrid ${1}_${2}_dwi_preprocessed.mif regrid -vox 1.25 dwi_preproc_upsampled.mif -debug 
+mrgrid ${1}_${2}_dwi_preprocessed.mif regrid -vox 1.25 dwi_preproc_upsampled.mif -info 
 
-#create mask of upsampled dwi, check for holes or too-liberal masking, use FSL-bet instead if needed
-dwi2mask dwi_preproc_upsampled.mif dwi_mask_upsampled.mif -debug
+#create mask of upsampled dwi, check mask output for holes/missing areas, a few slices will be missing from top and bottom due to cropping
+dwi2mask dwi_preproc_upsampled.mif dwi_mask_upsampled.mif -info
