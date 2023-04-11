0fc213fc5e3be16721b95abd12fb4bd0c4d6305f
Meaghan Perdue <meaghan.perdue@ucalgary.ca>
2023-04-11 16:48:43 -0600

    fix if else

--- src/run_script_preproc.sh
+++ src/run_script_preproc.sh
@@ -1,10 +1,13 @@
 # to run a single subject, pass subject id and session to processing script
 
-if [ -f "/Volumes/catherine_team/Trainee_Folders/mvperdue/preschool/bids_test/${1}/${2}/dwi/${1}_${2}_acq-b2000_dwi.nii.gz" ]
+if [ -f "/Volumes/catherine_team/Project_Folders/Preschool/preschool_bids/${1}/${2}/dwi/${1}_${2}_acq-b2000_dwi.nii.gz" ]
 then
     echo "run multishell"
     sh 1_preproc_multishell.sh ${1} ${2}
-else
+fi
+
+if [ -f "/Volumes/catherine_team/Project_Folders/Preschool/preschool_bids/${1}/${2}/dwi/${1}_${2}_acq-b750_dwi.nii.gz" ] && [ ! -f "/Volumes/catherine_team/Project_Folders/Preschool/preschool_bids/${1}/${2}/dwi/${1}_${2}_acq-b2000_dwi.nii.gz" ]
+then 
     echo "run single shell"
     sh 1_preproc_singleshell.sh ${1} ${2}
 fi
